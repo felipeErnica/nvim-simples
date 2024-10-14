be7cd9c1b0e874569282e05a8492f4ecc657980a
felipeErnica <felipe.ernica@gmail.com>
2024-10-14 03:16:26 -0300

    NeoTree mappings funcionando

--- lua/felipe/plugins/neo-tree.lua
+++ lua/felipe/plugins/neo-tree.lua
@@ -1,60 +1,74 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  keys = {
-    {
-      "<leader>fe",
-      function()
-        require("neo-tree.command").execute({
-          toggle = true,
-          source = "filesystem",
-          position = "left",
-        })
-      end,
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
     },
-  },
-  config = function ()
-    require("neo-tree").setup({
-      filesystem = {
-        follow_current_file = { enabled = true },
-        components = {
-          harpoon_index = function(config, node, _)
-            local harpoon_list = require("harpoon"):list()
-            local path = node:get_id()
-            local harpoon_key = vim.uv.cwd()
-
-            for i, item in ipairs(harpoon_list.items) do
-              local value = item.value
-              if string.sub(item.value, 1, 1) ~= "/" then
-                value = harpoon_key .. "/" .. item.value
-              end
-
-              if value == path then
-                vim.print(path)
-                return {
-                  text = string.format(" ⥤ %d", i), -- <-- Add your favorite harpoon like arrow here
-                  highlight = config.highlight or "NeoTreeDirectoryIcon",
-                }
-              end
-            end
-            return {}
-          end,
+    keys = {
+        {
+            "<leader>fe",
+            function()
+                require("neo-tree.command").execute({
+                    toggle = true,
+                    source = "filesystem",
+                    position = "left",
+                })
+            end,
         },
-        renderers = {
-          file = {
-            { "icon" },
-            { "name", use_git_status_colors = true },
-            { "harpoon_index" }, --> This is what actually adds the component in where you want it
-            { "diagnostics" },
-            { "git_status", highlight = "NeoTreeDimText" },
-          },
+        {
+            "<leader>F",
+            function()
+                require("neo-tree.command").execute({
+                    action = "focus",
+                    source = "filesystem",
+                    position = "left",
+                })
+            end,
         },
-      },
-    })
-  end
+    },
+    config = function ()
+        require("neo-tree").setup({
+            filesystem = {
+                follow_current_file = { enabled = true },
+                hijack_netrw_behavior = "open_current",
+                components = {
+                    harpoon_index = function(config, node, _)
+                        local harpoon_list = require("harpoon"):list()
+                        local path = node:get_id()
+                        local harpoon_key = vim.uv.cwd()
+
+                        for i, item in ipairs(harpoon_list.items) do
+                            local value = item.value
+                            if string.sub(item.value, 1, 1) ~= "/" then
+                                value = harpoon_key .. "/" .. item.value
+                            end
+
+                            if value == path then
+                                vim.print(path)
+                                return {
+                                    text = string.format(" ⥤ %d", i), -- <-- Add your favorite harpoon like arrow here
+                                    highlight = config.highlight or "NeoTreeDirectoryIcon",
+                                }
+                            end
+                        end
+                        return {}
+                    end,
+                },
+                renderers = {
+                    file = {
+                        { "icon" },
+                        { "name", use_git_status_colors = true },
+                        { "harpoon_index" }, --> This is what actually adds the component in where you want it
+                        { "diagnostics" },
+                        { "git_status", highlight = "NeoTreeDimText" },
+                    },
+                },
+            },
+            window = {
+                auto_expand_width = true
+            }
+        })
+    end
 }
