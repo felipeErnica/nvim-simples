9661e65757fee4fb890fc9de474b3e6e95e0c596
felipeErnica <felipe.ernica@gmail.com>
2024-10-24 00:14:17 -0300

    code actions UI

--- lua/felipe/plugins/fzf.lua
+++
@@ -1,8 +0,0 @@
-
-return {
-    'ibhagwan/fzf-lua',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    keys = function ()
-        vim.keymap.set("n", "<leader>ca" ,function () require('fzf-lua').lsp_code_actions() end, {silent = true})
-    end,
-}
